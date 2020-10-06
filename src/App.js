5ef3d41eb7c74bc57691cda8e53c849ae3ee84fd
adriancleung <adrian.leung@ualberta.ca>
2020-10-05 22:34:28 -0600

    feat: started creating react page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './styles.module.css';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import { socialMap } from './constants';
+
+function Title() {
+  const handleClick = (e, type) => {
+    e.preventDefault();
+    window.open(socialMap.get(type), '_self');
+  };
+
+  return (
+    <>
+    <div className={styles.App}>
+      <h1 className={styles.TitleText}>Adrian Leung</h1>
+      <div>
+        <GitHubIcon className={styles.SocialIcons} style={{ color: 'white', fontSize: 50 }} onClick={(e) => handleClick(e, 'github')} />
+        <LinkedInIcon className={styles.SocialIcons} style={{ color: 'white', fontSize: 50 }} onClick={(e) => handleClick(e, 'linkedin')} />
+        <MailOutlineIcon className={styles.SocialIcons} style={{ color: 'white', fontSize: 50 }} onClick={(e) => handleClick(e, 'email')} />
+      </div>
+    </div>
+    
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Title />
   );
 }
 
