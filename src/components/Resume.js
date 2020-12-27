21bad063b539a7577591112e91242e7bafde6e9c
Adrian Leung <adrian.leung@ualberta.ca>
2020-12-26 19:55:15 -0700

    Feat/resume section (#15)
    * fix: incorrect repo url used in api call (#13) (#14)
    
    * feat: add resume section
    fix: allow vertical scroll on horizontal scroll boundaries in projects

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Heading } from 'grommet';
 import { ScaleLoader } from 'react-spinners';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import axios from 'axios';
+import { SUCCESS_CODE } from '../constants';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const Resume = _props => {
-  // eslint-disable-next-line
   const [resumeLoading, setResumeLoading] = useState(true);
+  const [resume, setResume] = useState();
+
+  const fetchResume = () => {
+    axios
+      .get('https://api.adrianleung.dev/resume')
+      .then(res => {
+        if (res.status === SUCCESS_CODE) {
+          setResume(res.data);
+          setResumeLoading(false);
+        }
+      })
+      .catch(err => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchResume();
+  }, []);
 
   return (
     <Box height={'100%'}>
@@ -17,6 +38,24 @@ const Resume = _props => {
           radius={20}
           color={'white'}
         />
+        {!resumeLoading && (
+          <Document file={`data:application/pdf;base64,${resume}`}>
+            <Page
+              scale={
+                window.screen.width < 600
+                  ? 0.5
+                  : window.screen.width < 768
+                  ? 0.6
+                  : window.screen.width < 992
+                  ? 0.7
+                  : window.screen.width < 1200
+                  ? 0.8
+                  : 0.9
+              }
+              pageNumber={1}
+            />
+          </Document>
+        )}
       </Box>
     </Box>
   );
