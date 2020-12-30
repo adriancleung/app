094b95ffd4ad89ba9bac2d0a913f8f8158d9c4cf
adriancleung <adrian.leung@ualberta.ca>
2020-12-29 21:35:16 -0700

    feat: add mail to admin page

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 import { API_URL } from '../constants';
+import authHeader from '../utils/authHeader';
 
 export const sendMessage = async (firstName, lastName, email, message) => {
   try {
-    const res = await axios
-      .post(`${API_URL}/mail`, {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        message: message,
-      });
+    const res = await axios.post(`${API_URL}/mail`, {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      message: message,
+    });
     return res;
   } catch (err) {
     return err;
@@ -18,8 +18,16 @@ export const sendMessage = async (firstName, lastName, email, message) => {
 
 export const getResume = async () => {
   try {
-    const res = await axios
-      .get(`${API_URL}/resume`);
+    const res = await axios.get(`${API_URL}/resume`);
+    return res;
+  } catch (err) {
+    return err;
+  }
+};
+
+export const getAllMail = async () => {
+  try {
+    const res = await axios.get(`${API_URL}/mail`, { headers: authHeader() });
     return res;
   } catch (err) {
     return err;
