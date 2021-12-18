a4b5cf002e2bd3d734d8a2cf84db76cb0cb7d4d4
adriancleung <adrian.leung@ualberta.ca>
2021-12-18 22:33:32 +0000

    Deploying to gh-pages from  @ ed3b3c2f55a7b7f4fd0f26951880da9a71752c98 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.226ff21797d1a335892503eba263b76d.js"
+  "/precache-manifest.1391c8d7cc6a7731af1a790ac446143c.js"
 );
 
 self.addEventListener('message', (event) => {
